df7b34e98ee85ee3f148e3aa2d1c65fd8ea7ffb0
SowmyaRavi29 <sowmyaravi0429@gmail.com>
2025-12-30 16:01:29 +0530

    useCase2

---
+++ useCase1.java
@@ -0,0 +1,23 @@
+package oopsBannerApp;
+/**
+ * @author arumu
+ * @version
+ */
+public class useCase1 {
+    public static void main(String[] args) {
+        String[] banner = {
+                "   *         *      ******        ***",
+                " *   *     *   *    *     *     **",
+                "*     *   *     *   *      *  **",
+                "*     *   *     *   *     *     **",
+                "*     *   *     *   ******        **",
+                "*     *   *     *   *               **",
+                "*     *   *     *   *                 **",
+                " *   *     *   *    *              **",
+                "   *         *      *          ***" 
+        };
+        for (String c: banner){
+            System.out.println(c);
+        }
+    }
+}
