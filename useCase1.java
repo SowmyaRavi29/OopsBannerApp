3e7602172f3716ef009598983d2ca42eb9ebd1a1
SowmyaRavi29 <sowmyaravi0429@gmail.com>
2025-12-30 17:02:14 +0530

    UseCase3

--- useCase1.java
+++ useCase1.java
@@ -4,18 +4,61 @@ package oopsBannerApp;
  * @version
  */
 public class useCase1 {
+
     public static void main(String[] args) {
-        String[] banner = {
-                " *****     *****    ******    *****",
-                "*     *   *     *   *     *   *",
-                "*     *   *     *   *     *   *",
-                "*     *   *     *   ******    *****",
-                "*     *   *     *   *             *",
-                "*     *   *     *   *             *",
-                " *****     *****    *         *****" 
-        };
-        for (String c: banner){
-            System.out.println(c);
+        String[] banner = getResult();
+
+        for (String pattern : banner) {
+            System.out.println(pattern);
         }
+    }
+
+    public static String[] getResult() {
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        String[] result = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            result[i] = o[i] + "   " + o[i] + "    " + p[i] + "    " + s[i];
+        }
+        return result;
+    }
+
+    public static String[] getOPattern() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[]{
+                "******",
+                "*     *",
+                "*     *",
+                "******",
+                "*     ",
+                "*     ",
+                "*     "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+                "*****",
+                "*    ",
+                "*    ",
+                "*****",
+                "    *",
+                "    *",
+                "*****"
+        };
     }
 }
