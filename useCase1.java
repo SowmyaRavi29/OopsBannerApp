ba247ddb13ded98482f79c8830a33c5906d75c12
SowmyaRavi29 <sowmyaravi0429@gmail.com>
2025-12-30 16:17:17 +0530

    Updated useCase2

--- useCase1.java
+++ useCase1.java
@@ -6,15 +6,13 @@ package oopsBannerApp;
 public class useCase1 {
     public static void main(String[] args) {
         String[] banner = {
-                "   *         *      ******        ***",
-                " *   *     *   *    *     *     **",
-                "*     *   *     *   *      *  **",
-                "*     *   *     *   *     *     **",
-                "*     *   *     *   ******        **",
-                "*     *   *     *   *               **",
-                "*     *   *     *   *                 **",
-                " *   *     *   *    *              **",
-                "   *         *      *          ***" 
+                " *****     *****    ******    *****",
+                "*     *   *     *   *     *   *",
+                "*     *   *     *   *     *   *",
+                "*     *   *     *   ******    *****",
+                "*     *   *     *   *             *",
+                "*     *   *     *   *             *",
+                " *****     *****    *         *****" 
         };
         for (String c: banner){
             System.out.println(c);
